7d092a830e8e0bab0a004a9424380d1247835afd
lintsuihin <quincylin.333@gmail.com>
2022-08-16 22:37:01 +0800

    Added docstrings for portfolio

--- futubot/portfolio.py
+++ futubot/portfolio.py
@@ -8,6 +8,18 @@ from .stockframe import StockFrame
 
 
 class Portfolio(Accounts):
+    """Implementation of investor's portfolio.
+
+    Args:
+        host (str): FutuOpenD listening address. Default: '127.0.0.1'.
+        port (int): FutuOpenD listening port. Default: 11111.
+        filter_trdmarket (TrdMarket): Filter accounts according to the
+            transaction market authority. Default: TrdMarket.HK.
+        security_firm (SecurityFirm): Specified security firm.
+            Default: SecurityFirm.FUTUSECURITIES.
+        paper_trading (bool): Whether to enable paper trading or not.
+            Default: False.
+    """
     def __init__(self,
                  host='127.0.0.1',
                  port=11111,
@@ -26,7 +38,25 @@ class Portfolio(Accounts):
         self._stockframe_daily = None
 
     def get_portfolio_info(self):
+        """Get basic information of portfolio.
 
+        This function gets the basic information of portfolio
+        including total assets, available cash, total market
+        value of the holdings, total invested value of the holdings
+        and the total profit and loss.
+
+        Returns:
+            portfolio_info (dict[float]): A dict of portfolio info
+                with the following keys:
+                    total_assets (float): The total value of assets.
+                    cash (float): The available cash buying power.
+                    total_market_value (float): The total market value
+                        of current holdings.
+                    total_invested_value (float): The total invested
+                        value of current holdings.
+                    total_pnl_value (float): The absolute profit and
+                        loss value of current holdings.
+        """
         portfolio_info = {}
 
         account_info = self.get_account_info()
@@ -55,10 +85,27 @@ class Portfolio(Accounts):
 
     @property
     def portfolio_info(self):
+        """Getter of portfolio_info property."""
         return self._portfolio_info
 
     def add_position(self, code, stock_name, quantity):
+        """Add a position to the portfolio.
 
+        This function adds the code, name and quantity
+        of holding security to the portfolio positions. If
+        the code is in stocks of interest but is not currently
+        held by the trading account, the holding quantity is
+        set to zero.
+
+        Args:
+            code (str): The code of security.
+            stock_name (str): The name of security.
+            quantity (int | float): The holding quantity.
+
+        Returns:
+            (dict) A dict of holding positions with keys 'code'
+                and values 'code', 'stock_name', 'qty'.
+        """
         if not isinstance(code, str):
             raise TypeError(f'Only str type is supported for code, '
                             f'but got {type(code)}')
@@ -80,7 +127,16 @@ class Portfolio(Accounts):
         return self.positions
 
     def remove_position(self, code):
+        """Remove position of code from portfolio.
 
+        Args:
+            code (str): The code to be removed from
+                portfolio.
+
+        Returns:
+            bool: True if code is removed from portfolio,
+                otherwise False.
+        """
         if not isinstance(code, str):
             raise TypeError(f'Only str type is supported for code, '
                             f'but got {type(code)}')
@@ -94,7 +150,16 @@ class Portfolio(Accounts):
 
     @property
     def holdings(self):
+        """Getter and setter for holdings property.
 
+        This function sets the values in holdings dict
+        to be the current holding quantities of codes in
+        portfolio. It also serves as a getter for the holdings
+        dict.
+
+        Returns:
+            (dict[int | float]): A dict of current holding quantities.
+        """
         for code in self.positions.keys():
             self._holdings[code] = self.positions[code]['qty']
 
@@ -110,7 +175,18 @@ class Portfolio(Accounts):
     #     return holdings
 
     def update_positions(self, order_infos):
+        """Update positions in portfolio based on order information.
 
+        This function updates the cost price and holding quantity of
+        positions when trading orders are present. Note that we directly
+        update the positions dict of the Portfolio object instead
+        of updating positions using the Futu API position_list_query()
+        since it is found that the API has certain latency and
+        therefore does not update the positions in real time.
+
+        Args:
+            order_infos (dict[dict]): A dict of order info.
+        """
         if not isinstance(order_infos, dict):
             raise TypeError(f'Only dict type is supported for order_infos, '
                             f'but got {type(order_infos)}')
@@ -141,7 +217,19 @@ class Portfolio(Accounts):
     #             self.positions[code]["qty"] = existing_positions[code]["qty"]
 
     def calculate_portfolio_weights(self):
+        """Calculate the weights of holdings in portfolio.
 
+        The weight of a given stock is the ratio between the
+        market value of the stock and the total market value.
+        If the stock is in stocks of interest but is not
+        currently held by the account, a zero weight is assigned
+        to the stock.
+
+        Returns:
+            weights (dict[float]): The weights of holdings in
+                portfolio. The keys are the codes of holdings
+                and the weights are expressed in ratio terms.
+        """
         weights = {}
         weights['cash'] = 1.00
 
@@ -168,7 +256,27 @@ class Portfolio(Accounts):
         return weights
 
     def portfolio_variance(self, weights, covariance_matrix):
+        """Calculate the portfolio variance.
 
+        The variance of portfolio is a measure of the overall risk
+        of portfolio. It is the spread of the actual returns of the
+        securities that constitute the portfolio. The formula of
+        portfolio variance is:
+
+            portfolio_var = weights^T * covariance_matrix * weights
+
+        where weights^T is the transpose of weights vector.
+
+        Args:
+            weights (dict[float]): The weights of holdings in
+                portfolio with length n, where n is the number of holdings.
+            covariance_matrix (pd.DataFrame): The n x n covariance matrix of
+                the log daily returns of holdings in portfolio, where n is
+                the number of holdings.
+
+        Returns:
+            portfolio_variance (float): The portfolio variance.
+        """
         if not isinstance(weights, dict):
             raise TypeError(f'Only dict type is supported for weights, '
                             f'but got {type(weights)}')
@@ -187,7 +295,18 @@ class Portfolio(Accounts):
         return portfolio_variance
 
     def portfolio_weighted_returns(self, portfolio_metrics):
+        """Calculate the total weighted returns of portfolio.
 
+        This function calculates the total returns of portfolio weighted
+        by the respective market value of each holding.
+
+        Args:
+            portfolio_metrics (dict[dict]): A dict of portfolio metrics.
+
+        Returns:
+            portfolio_weighted_returns: The total weighted returns of
+                portfolio.
+        """
         if not isinstance(portfolio_metrics, dict):
             raise TypeError(
                 f'Only dict type is supported for portfolio_metrics, '
@@ -205,7 +324,22 @@ class Portfolio(Accounts):
                                 average_returns,
                                 variance,
                                 risk_free_rate=0.00):
+        """Calculate the annualized Sharpe Ratio of portfolio.
 
+        The (annualized) Sharpe Ratio is a defined as the (annual)
+        average return in excess of risk-free rate per unit of
+        volatility. A portfolio with a Sharpe Ratio of 1.5 or greater
+        is considered good.
+
+        Args:
+            average_returns (float): The average weighted daily return.
+            variance (float): The portfolio variance.
+            risk_free_rate (float): The rate of return of a zero-risk
+                investment (e.g. government bonds). Default: 0.00.
+
+        Returns:
+            annaualized_sharpe_ratio (float): The annualized Sharpe Ratio.
+        """
         if not isinstance(average_returns, (int, float)):
             raise TypeError(
                 f'Only int or float type is supported for average_returns, '
@@ -224,13 +358,37 @@ class Portfolio(Accounts):
 
         standard_deviation = variance**0.5
 
+        # Annualize by multiply by 252 trading days per year.
         annualized_sharpe_ratio = abs(
             average_returns - risk_free_rate) / standard_deviation * 252**0.5
 
         return annualized_sharpe_ratio
 
     def calculate_portfolio_metrics(self):
+        """Calculate common metrics that measure portfolio performance.
 
+        Returns:
+            portfolio_metrics (dict[dict]): A dict of portfolio metrics. The
+                outer dict has keys 'portfolio' and 'codes'. For each code, the
+                inner dict contains the following keys:
+                    average_returns (float): The average log daily return.
+                    covariance_of_returns (dict[dict]): The covariance of
+                        returns with respect to other holdings.
+                    standard_deviation_of_returns (float): The standard
+                        deviation of log daily returns.
+                    variance_of_returns (float): The variance of log daily
+                        returns.
+                    weight (float): The weight of holding.
+                    weighted_returns (float): The average log daily return
+                        multipliednby the weight of holding.
+                For the portolio key, the inner dict contains the following
+                keys:
+                    annualized_sharpe_ratio (float): The annualized Sharpe
+                        Ratio of portfolio.
+                    average_returns (float): The weighted average return of
+                        portfolio.
+                    variance (float): The variance of portfolio.
+        """
         if not self._stockframe_daily:
             self._get_daily_historical_prices()
 
@@ -293,7 +451,20 @@ class Portfolio(Accounts):
         return portfolio_metrics
 
     def _get_daily_historical_prices(self, ktype=KLType.K_DAY):
+        """Get the daily historical prices of holdings in portfolio.
 
+        The daily historical prices are used to calculate the common
+        metrics that measure portfolio performance. The end_date of
+        the historical prices is the current date, and the start_date
+        is one year behind the end_date.
+
+        Args:
+            ktype (KLType): The type of candlestick. Default: K_DAY.
+
+        Returns:
+            (StockFrame): A StockFrame object of historical prices of
+                all holdings in portfolio.
+        """
         historical_prices = []
 
         code_list = list(self.positions.keys())
@@ -304,7 +475,6 @@ class Portfolio(Accounts):
         start_date = start_date.strftime('%Y-%m-%d %H:%M:%S')
 
         for code in code_list:
-
             historical_quotes = self.get_historical_candles(
                 code=code,
                 start=start_date,
