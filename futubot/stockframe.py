d5f01b15cce619a0478c64172301a75a01a6b7b1
lintsuihin <quincylin.333@gmail.com>
2022-08-17 01:23:18 +0800

    add docstrings for stockframe

--- futubot/stockframe.py
+++ futubot/stockframe.py
@@ -2,53 +2,101 @@ import pandas as pd
 
 
 class StockFrame:
+    """Implementation of a dataframe of stock data.
+
+    Args:
+        data (list[dict]): A list of historical quotes data
+            to be converted to a dataframe.
+    """
     def __init__(self, data):
         self.data = data
         self._frame = self.create_frame()
         self._code_groups = None
-        # self.code_rolling_groups = self.create_code_rolling_groups()
 
     @property
     def frame(self):
+        """Getter for the frame property."""
         return self._frame
 
     @property
     def code_groups(self):
+        """Setter and getter for the code_groups property.
 
+        The code_groups property groups the stockframe dataframe
+        by 'code'.
+
+        Returns:
+            (pd.DataFrameGroupBy) -- A pandas DataFrameGroupBy object grouped
+                by code.
+        """
         self._code_groups = self.frame.groupby(by='code',
                                                as_index=False,
                                                sort=True)
 
         return self._code_groups
 
-    def create_code_rolling_groups(self, size):
-
-        code_rolling_groups = self.code_groups.rolling(size)
-        return code_rolling_groups
-
     def create_frame(self):
+        """Create a MultiIndex pandas dataframe for data.
 
+        The dataframe has index (code, time_key) and columns:
+        'open', 'close', 'high', 'low', 'volume'.
+
+        Returns:
+            price_df (pd.DataFrame): A MultiIndex pandas
+                dataframe.
+        """
         price_df = pd.DataFrame(data=self.data)
-        # price_df = self._parse_datetime_column(price_df=price_df)
+        price_df = self._parse_time_key_column(price_df=price_df)
         price_df = self._set_multi_index(price_df=price_df)
 
         return price_df
 
-    def _parse_datetime_column(self, price_df):
+    def _parse_time_key_column(self, price_df):
+        """Convert time_key column to datetime object.
 
-        price_df['datetime'] = pd.to_datetime(price_df['time_key'],
+        This function parses the time_key column to a pandas
+        datetime object with origin 'unix'.
+
+        Args:
+            price_df (pd.DataFrame): A pandas dataframe of historical
+                candlesticks with time_key column of type 'str'.
+
+        Returns:
+            price_df (pd.DataFrame): A pandas dataframe of historical
+                candlesticks with time_key column of type 'datetime'.
+        """
+        price_df['time_key'] = pd.to_datetime(price_df['time_key'],
                                               origin='unix')
 
         return price_df
 
     def _set_multi_index(self, price_df):
+        """Set the index of dataframe to be MultiIndex.
 
+        The index of dataframe is set to be (code, time_key).
+
+        Args:
+            price_df (pd.DataFrame): A pandas dataframe with
+                default numerical index.
+
+        Returns:
+            price_df (pd.DataFrame): A MultiIndex dataframe with
+                index (code, time_key).
+        """
         price_df = price_df.set_index(keys=['code', 'time_key'])
 
         return price_df
 
     def add_rows(self, data):
+        """Add new rows to dataframe.
 
+        This function converts the data list into a pandas
+        Series and then adds to the existing MultiIndex dataframe as
+        new rows.
+
+        Args:
+            data (list[dict]): A list of new candlestick data.
+        """
         if not isinstance(data, list):
             raise TypeError(f'Only list type is supported for data, '
                             f'but got {type(data)}')
