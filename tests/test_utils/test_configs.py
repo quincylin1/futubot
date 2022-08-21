a743913b294a03a647bce853398af5922db0473a
lintsuihin <quincylin.333@gmail.com>
2022-08-21 23:56:53 +0800

    fix test config

--- tests/test_utils/test_configs.py
+++ tests/test_utils/test_configs.py
@@ -7,10 +7,10 @@ from utils.config import Config
 def test_parse_config(cfg_file):
     with pytest.raises(TypeError):
         Config.parse_config(123456)
-    
+
     with pytest.raises(OSError):
         Config.parse_config('demo/config.py')
-    
+
     cfg_dict = Config.parse_config(cfg_pth=cfg_file)
 
     assert cfg_dict['account']['filter_trdmarket'] == 'HK'
